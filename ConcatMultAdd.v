2b6a13ff30e162ba9c6b1a14acfc33043580127e
abdullahsahruri <71990122+abdullahsahruri@users.noreply.github.com>
2023-05-01 13:28:36 -0500

    Update ConcatMultAdd.v
    Update parameters to Data_width = 8, Frac_width=4

--- ConcatMultAdd.v
+++ ConcatMultAdd.v
@@ -1,16 +1,16 @@
 module ConcatMultAdd(X, h_in, W0, W1, b, out);	
 	// Concatenates and Adds
 	// incoming data signed and fixed width
-	parameter DATA_WIDTH = 16;
-	parameter FRACT_WIDTH = 8;
+	parameter DATA_WIDTH = 8;
+	parameter FRACT_WIDTH = 4;
 	
 	input signed [DATA_WIDTH-1:0] X, h_in;
 	input signed [DATA_WIDTH-1:0] W0, W1, b;
 	output signed [DATA_WIDTH-1:0] out;
 
 	// internal regs/wires
-	wire signed [DATA_WIDTH+DATA_WIDTH-1:0] p1,p2;
-	wire signed [DATA_WIDTH+DATA_WIDTH-1:0] temp;
+	wire signed [DATA_WIDTH-1:0] p1,p2;
+	wire signed [DATA_WIDTH-1:0] temp;
 	wire cout, cout2;
 	
 	// behavior: out = W*{x,h_in} + b where W={W0,W1}
